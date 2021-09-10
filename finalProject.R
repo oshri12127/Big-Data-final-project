499ce5ad5cb056618610a7f60a75e712bbc79c4f
Oshri <48412341+oshri12127@users.noreply.github.com>
2021-09-10 10:08:29 +0300

    Delete finalProject.R

--- finalProject.R
+++
@@ -1,9 +0,0 @@
-
-library(data.table)
-library(dplyr)
-library(ggplot2)
-
-dt.president = fread("1976-2020-president.csv")
-dt.education = fread("EducationReport.csv")
-dt.mortality = fread("US county-level mortality.csv")
-dt.unemployment = fread("UnemploymentReport.csv")
